e1734f6be4c6e71b2df718b71b9f2574cf5802d6
Aryan Chouksey <ayushchauksey08@gmail.com>
2024-07-17 20:18:38 +0530

    LogIn, Home and Profile done.

--- app/index.jsx
+++ app/index.jsx
@@ -33,11 +33,11 @@ export default function App() {
             It's time for a fresh haircut!
           </Text>
 
-          {/* <CustomButton 
+          <CustomButton 
             title="Continue with email"
             handlePress={() => router.push('/sign-in')}
             containerStyles="w-full mt-7"
-          /> */}
+          />
         </View>
       </ScrollView>
 
